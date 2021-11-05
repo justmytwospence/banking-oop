544ac36a565bdf55c0b42ed411c07166cec33396
Spencer Boucher <github@spencerboucher.com>
2021-11-05 12:07:48 -0500

    Add entry_point to setup.py

--- setup.py
+++ setup.py
@@ -3,6 +3,10 @@ from setuptools import setup, find_packages
 setup(
     name='banking',
     version='0.1',
-    packages=find_packages(),
-    long_description=open('README.md').read()
+    py_modules=['banking'],
+    long_description=open('README.md').read(),
+    entry_points='''
+        [console_scripts]
+        banking=banking.__main__:cli
+    '''
 )
